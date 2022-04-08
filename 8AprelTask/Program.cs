a35e038215257e14918d52d0b99ade1ee9770a5f
unknown <tu7d5w0m5@code.edu.az>
2022-04-08 00:32:08 -0800

    first commit

---
+++ 8AprelTask/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8AprelTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            
+
+            bool activeMenu = true;
+            do
+            {
+                Menu();
+                string choice = Console.ReadLine();
+                if(choice == "0")
+                    activeMenu = false;
+                else
+                {
+                    int no;
+                    string examName;
+                    Student newStudent;
+                    switch (choice)
+                    {
+
+                        case "1":
+                            Console.WriteLine("Fullname :");
+                            string fullName = Console.ReadLine();
+                            Student student = new Student()
+                            {
+                                FullName = fullName,
+                            };
+                            students.Add(student);
+                            break;
+                        case "2":
+                            Console.WriteLine("No :");
+                            no = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Exam name :");
+                            examName = Console.ReadLine();
+                            Console.WriteLine("Point :");
+                            int point = int.Parse(Console.ReadLine());
+                            newStudent = students.Find(x => x.No == no);
+                            newStudent.AddExam(examName, point);
+                            break;
+                        case "3":
+                            Console.WriteLine("No :");
+                            no = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Exam name :");
+                            examName = Console.ReadLine();
+                            newStudent = students.Find(x => x.No == no);
+                            Console.WriteLine(newStudent._exams[examName]);
+                            break;
+                        case "4":
+                            Console.WriteLine("No :");
+                            no = int.Parse(Console.ReadLine());
+                            Student newStudent2 = students.Find(x => x.No == no);
+                            foreach (var item in newStudent2._exams)
+                            {
+                                Console.WriteLine(item.Key);
+                                Console.WriteLine(item.Value);
+                                Console.WriteLine("------");
+                            }
+                            break;
+                        case "5":
+                            Console.WriteLine("No :");
+                            no = int.Parse(Console.ReadLine());
+                            Student newStudent3 = students.Find(x => x.No == no);
+                            Console.WriteLine(newStudent3.GetExamAvg());
+                            break;
+                        case "6":
+                            Console.WriteLine("No :");
+                            no = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Exam name :");
+                            examName = Console.ReadLine();
+                            Student newStudent4 = students.Find(x => x.No == no);
+                            newStudent4._exams.Remove(examName);
+                            break;
+
+                        default:
+                            Console.WriteLine("Bu secim yoxdur !");
+                            break;
+                    }
+                }
+                
+            } while (activeMenu);
+            
+
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Menu :");
+            Console.WriteLine("1. Tələbə əlavə et");
+            Console.WriteLine("2. Tələbəyə imtahan əlavə et");
+            Console.WriteLine("3. Tələbənin bir imtahan balına bax");
+            Console.WriteLine("4. Tələbənin bütün imtahanlarını göstər");
+            Console.WriteLine("5. Tələbənin imtahan ortalamasını göstər");
+            Console.WriteLine("6. Tələbədən imtahan sil");
+            Console.WriteLine("0. Proqramı bitir");
+        }
+    }
+}
+
---
+++ 8AprelTask/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8AprelTask
+{
+    internal class Student
+    {
+        private static int _no;
+        public Student()
+        {
+            _no++;
+            this.No = _no;
+        }
+        public int No { get; }
+        public string FullName { get; set; }
+        public Dictionary<string, int> _exams = new Dictionary<string, int>();
+        public void AddExam(string examName,int point)
+        {
+            if (!_exams.ContainsKey(examName))
+            {
+                _exams.Add(examName, point);
+            }
+        }
+        public int GetExamResult(string examName)
+        {
+            if (_exams.ContainsKey(examName))
+            {
+                return _exams[examName];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public int GetExamAvg()
+        {
+            if (_exams.Count == 0)
+            {
+                return -1;
+            }
+            int avgSum = 0;
+            foreach (var item in _exams)
+            {
+                avgSum += item.Value;
+            }
+            return (avgSum)/_exams.Count;
+        }
+    }
+}
